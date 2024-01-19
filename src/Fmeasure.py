e972c6d68667c8f118e1b688ba9472d9b42263cd
pdledger <p.d.ledger@swansea.ac.uk>
2024-01-19 15:25:50 +0000

    fmeasure.py

--- src/Fmeasure.py
+++ src/Fmeasure.py
@@ -2,8 +2,9 @@ import numpy as np
 def Fmeasure(sorteigenvalues,SortedURstore, SortedUIstore, SortedQRstore, SortedQIstore, SortedKstore, Rstore,Istore, Frequencies):
     N=len(Frequencies)
     Fexactconst = np.zeros(N)
-    Fapproxconst = np.zeros(N)
-
+    #Fapproxconst = np.zeros(N)
+    Fapproxconst_min = np.zeros(N)
+    Fapproxconst_max = np.zeros(N)
     # First compute F using the exact constant
     for n in range(N):
         QR = np.zeros((3,3))
@@ -46,11 +47,24 @@ def Fmeasure(sorteigenvalues,SortedURstore, SortedUIstore, SortedQRstore, Sorted
         for i in range(3):
             diffeig+=np.abs(uR[i]-uI[i])**2
 
-        normalisationapprox=0
-        for j in range(3):
-            normalisationapprox-=2*(uI[j]*uR[j])/np.sqrt(3)
-        normalisationapprox+=((uI[1]*uR[0])+(uI[2]*uR[0])+(uI[0]*uR[1])+(uI[2]*uR[1])+(uI[0]*uR[2])+(uI[1]*uR[2]))/np.sqrt(3)
+        #normalisationapprox=0
+        #for j in range(3):
+        #    normalisationapprox-=2*(uI[j]*uR[j])/np.sqrt(3)
+        #normalisationapprox+=((uI[1]*uR[0])+(uI[2]*uR[0])+(uI[0]*uR[1])+(uI[2]*uR[1])+(uI[0]*uR[2])+(uI[1]*uR[2]))/np.sqrt(3)
+        evlist = np.zeros(3)
+        evlist[0]= -uI[1]*uR[1]-uI[2]*uR[2]+uI[2]*uR[1]+uI[1]*uR[2]
+        evlist[1]= -uI[0]*uR[0]-uI[2]*uR[2]+uI[2]*uR[0]+uI[0]*uR[2]
+        evlist[2]= -uI[1]*uR[1]-uI[0]*uR[0]+uI[1]*uR[0]+uI[0]*uR[1]
 
-        Fapproxconst[n] = np.abs(np.linalg.norm(R-I,ord='fro')**2 - diffeig) / np.abs(normalisationapprox)
-    Fapproxconst= np.sqrt(Fapproxconst)
-    return Fexactconst,Fapproxconst
+        normalisation_min = np.min(evlist)
+        normalisation_max = np.max(evlist)
+        Fapproxconst_min[n] = np.abs(np.linalg.norm(R-I,ord='fro')**2 - diffeig) / np.abs(normalisation_min)
+        Fapproxconst_max[n] = np.abs(np.linalg.norm(R-I,ord='fro')**2 - diffeig) / np.abs(normalisation_max)
+
+#        Fapproxconst[n] = np.abs(np.linalg.norm(R-I,ord='fro')**2 - diffeig) / np.abs(normalisationapprox)
+#    Fapproxconst= np.sqrt(Fapproxconst)
+    Fapproxconst_min=np.sqrt(Fapproxconst_min)
+    Fapproxconst_max=np.sqrt(Fapproxconst_max)
+
+    return Fexactconst,Fapproxconst_min,Fapproxconst_max
+    #Fapproxconst
