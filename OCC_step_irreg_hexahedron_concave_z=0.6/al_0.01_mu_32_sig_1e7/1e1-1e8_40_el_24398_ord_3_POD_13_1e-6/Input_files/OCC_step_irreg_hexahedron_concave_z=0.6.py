75542d7d6a5cc1d413b3b61ca53992c1c22b6606
Paul Ledger <paulledger@Pauls-MacBook-Pro.local>
2024-02-19 18:13:17 +0000

    Paul update 19th Feb 6.14pm

---
+++ OCC_step_irreg_hexahedron_concave_z=0.6/al_0.01_mu_32_sig_1e7/1e1-1e8_40_el_24398_ord_3_POD_13_1e-6/Input_files/OCC_step_irreg_hexahedron_concave_z=0.6.py
@@ -0,0 +1,42 @@
+from netgen.occ import *
+
+"""
+James Elgy - 2023:
+Two joined tetrahedra for Netgen OCC geometry loaded from existing step geometry.
+"""
+
+material_name = ['hex']
+sigma = [1e7]
+mur = [32]
+alpha = 0.01
+
+# Setting Boundary layer Options:
+max_target_frequency = 1e8
+boundary_layer_material = material_name[0]
+number_of_layers = 3
+
+
+geo = OCCGeometry(r'StepFiles/Hex_concave-z=0.6.step')
+hex = geo.shape.Move((-geo.shape.center.x, -geo.shape.center.y, -geo.shape.center.z))
+
+hex.bc('default')
+hex.mat(material_name[0])
+hex.maxh = 0.08
+
+sph = Sphere(Pnt(0,0,0), r=100)
+sph.mat('air')
+sph.bc('outer')
+sph.maxh=1000
+
+joined_object = Glue([sph, hex])
+nmesh = OCCGeometry(joined_object).GenerateMesh(meshsize.coarse)
+
+# # Applying Boundary Layers:
+mu0 = 4 * 3.14159 * 1e-7
+tau = (2/(max_target_frequency * sigma[0] * mu0 * mur[0]))**0.5 / alpha
+layer_thicknesses = [(2**n)*tau for n in range(number_of_layers)]
+
+nmesh.BoundaryLayer(boundary=".*", thickness=layer_thicknesses, material=boundary_layer_material, domains=boundary_layer_material, outside=False)
+
+
+nmesh.Save(r'VolFiles/OCC_step_irreg_hexahedron_concave_z=0.6.vol')
