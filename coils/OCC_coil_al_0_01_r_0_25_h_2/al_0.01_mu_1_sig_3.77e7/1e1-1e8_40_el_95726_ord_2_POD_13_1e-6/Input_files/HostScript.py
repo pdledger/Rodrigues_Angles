23364d4b0fc92179e648988b21a8397b33c575dd
P.D. Ledger <p.d.ledger28@gmail.com>
2025-04-01 20:12:33 +0100

    Paul update 1st April 2025

---
+++ coils/OCC_coil_al_0_01_r_0_25_h_2/al_0.01_mu_1_sig_3.77e7/1e1-1e8_40_el_95726_ord_2_POD_13_1e-6/Input_files/HostScript.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+#from contextlib import chdir
+
+#with chdir(Your_Path):
+# do stuff here
+from main import main
+import time
+
+if __name__ == '__main__':
+    comparison_eig_p = np.zeros((40,4), dtype=complex)
+    comparison_ndofs_p = np.zeros(4)
+    CPUs=[4,3,2,2,1]
+    for p in [0,1,2,3,4]:
+        print("order running = ",p)
+        Return_Dict = main(geometry='OCC_coil_al_0_01_r_0_25_h_2.py',cpus=CPUs[p],use_OCC=True, use_POD=True, order=p)
